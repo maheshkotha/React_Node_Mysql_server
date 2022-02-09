bd7f76a3327238549d5317b1a996ecd2a5a4455b
Mahesh Babu <mahesh96.kotha@gmail.com>
2022-02-10 00:32:10 +0530

    changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("server");
+const express = require("express");
+const db = require("./models");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+// ======= Routers ========
+const postRouter = require("./routes/Posts");
+
+app.use("/posts", postRouter);
+
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on port 3001");
+  })
+})
---
+++ routes/Posts.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { Posts } = require("./../models");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const listofPosts = await Posts.findAll();
+  res.json(listofPosts);
+});
+
+router.get("/byId/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findByPk(id);
+  res.json(post);
+})
+
+router.post("/", async (req, res) => {
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
+ });
+
+
+
+module.exports = router;
